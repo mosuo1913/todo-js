06fb63c1339d0fa2450356b042b1aa4463b19c6d
mosuo1913 <44077303+mosuo1913@users.noreply.github.com>
2021-09-21 08:08:42 +0900

    タスクの戻す機能作成

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  createIncompleteList(inputText);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
   // liタグ生成
   const li = document.createElement("li");
 
@@ -14,7 +24,7 @@ const onClickAdd = () => {
 
   // pタグ生成
   const p = document.createElement("p");
-  p.innerText = inputText;
+  p.innerText = text;
 
   // button(完了)タグ生成
   const completeButton = document.createElement("button");
@@ -27,7 +37,7 @@ const onClickAdd = () => {
     const addTarget = completeButton.parentNode.parentNode;
 
     // TODO内容テキストを取得
-    const text = addTarget.firstChild.firstChild.innerText;
+    const text = completeButton.parentNode.firstChild.innerText;
 
     // div以下を初期化
     addTarget.firstChild.textContent = null;
@@ -36,9 +46,19 @@ const onClickAdd = () => {
     const p = document.createElement("p");
     p.innerText = text;
 
-    // buttonタグ生成
+    // button(戻す)タグ生成
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // 押された戻すボタンの親タグ(div)の親タグ(li)を完了リストから削除
+      const deleteTarget = backButton.parentNode.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      // TODO内容テキストを取得
+      const text = backButton.parentNode.firstChild.innerText;
+
+      createIncompleteList(text);
+    });
 
     // divタグの子要素に各要素を設定
     addTarget.firstChild.appendChild(p);
@@ -66,11 +86,6 @@ const onClickAdd = () => {
 
   // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(li);
-};
-
-// 未完了リストから指定の要素を削除
-const deleteFromIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
